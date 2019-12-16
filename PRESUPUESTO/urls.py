af7e0ac8966cab29e622acd425f842f010e0c74a
Eduardotx <42276156+Eduardotx@users.noreply.github.com>
2019-12-15 22:48:25 -0300

    Update and rename PRESUPUESTO/urls.py to  PRESUPUESTO/urls.py

--- PRESUPUESTO/urls.py
+++ PRESUPUESTO/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('CORE.urls'))
+    path('', include('CORE.urls')),
+    path('', include('pwa.urls')),
 ]
+
+
+admin.site.site_header="Administracion FreshAir"
+admin.site.site_title="FreshAir"
+admin.site.index_title="Administración"
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
